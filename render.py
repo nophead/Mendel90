d7213757c43aa13eb6233f2d9a923539e0032374
Simon Kirkby <zignig@bl3dr.com>
2012-03-16 10:54:53 +0800

    added seperator , so it is multi platform

--- render.py
+++ render.py
@@ -3,18 +3,18 @@ import sys
 import shutil
 
 def render(machine):
-	render_dir = machine + "/render"
+	render_dir = machine + os.sep + "render"
 	print render_dir 
 	try:
 		os.stat(render_dir)
 	except:
 		os.mkdir(render_dir)
-	li = os.listdir(machine+'/stls')
+	li = os.listdir(machine+os.sep+'stls')
 	stls = []
 	for i in li:
 		stls.append(i[:-4])
 	for i in stls:
-		command = 'blender -P utils/viz.py -- '+machine+'/stls/'+i+'.stl '+machine+'/render/'+i+'.png'
+		command = 'blender utils'+os.sep+'render.blender -P utils'+os.sep+'viz.py -- '+machine+os.sep+'stls'+os.sep+i+'.stl '+machine+os.sep+'render'+os.sep+i+'.png'
 		print command
 
 if __name__ == '__main__':
