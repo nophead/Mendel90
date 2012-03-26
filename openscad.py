2de0d1aa22d311bf960ce3f750cac5edfcbf4678
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2012-03-26 19:26:30 +0200

    Revert the compatibility changes for OpenSCAD 2011.06.

--- openscad.py
+++ openscad.py
@@ -1,33 +1,11 @@
 import subprocess
 import os
 
-version = "unknown"
-
-if not os.name == "nt":
-    try:
-        log = open("openscad.log", "w")
-        subprocess.call(["openscad","--version"],stdout = log)
-        log.close()
-        version = open("openscad.log", "r").read().split()[2]
-    except OSError, e:
-        print "OpenSCAD could not be executed"
-
 def run(*args):
-    args = list(args)
     if os.name == "nt":
-        subprocess.call(["openscad_cl"] + args)
+        subprocess.call(["openscad_cl"] + list(args))
     else:
-        if version == "2011.06":
-            #-o attempts to output a OFF in this version, so we have to change the switches
-            if "-o" in args:
-                idx = args.index("-o")
-                ext = args[idx+1].split('.')[-1]
-                if  ext == "dxf":
-                    args[idx] = "-x"
-                elif ext == "stl":
-                    args[idx] = "-s"
-
         log = open("openscad.log", "w")
-        subprocess.call(["openscad"] + args, stdout = log, stderr = log)
+        subprocess.call(["openscad"] + list(args), stdout = log, stderr = log)
         log.close()
 
